5f534e6eda042dc4b7a25cb4e7ce284b600ac2e4
miguelmunoz2019 <michaelalmos10@gmail.com>
2019-11-05 17:33:04 -0500

    avances

--- Productos/logic/logic_producto.py
+++ Productos/logic/logic_producto.py
@@ -8,3 +8,14 @@ def create_producto(form):
     producto = form.save()
     producto.save()
     return ()
+
+def edit_producto(form):
+    producto = form.save()
+    producto.save()
+    return ()
+
+
+def delete_producto(id):
+    Producto.objects.all.remove(id)
+    queryset = Producto.objects.all().order_by('nombre')
+    return (queryset)
--- Productos/views.py
+++ Productos/views.py
@@ -3,7 +3,8 @@ from .forms import ProductoForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .logic.logic_producto import create_producto, get_productos
+from .logic.logic_producto import create_producto, get_productos, edit_producto, delete_producto
+
 
 def producto_list(request):
     productos = get_productos()
@@ -29,3 +30,39 @@ def producto_create(request):
     }
 
     return render(request, 'Producto/ProductoCreate.html', context)
+
+def producto_edit(request):
+
+    if request.method == 'PUT':
+        form = ProductoForm(request.PUT)
+        if form.is_valid():
+            edit_producto(form)
+            messages.add_message(request, messages.SUCCESS, 'Producto edit successful')
+            return HttpResponseRedirect(reverse('productoEdit'))
+        else:
+            print(form.errors)
+    else:
+        form = ProductoForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'Producto/ProductoEdit.html', context)
+
+
+def producto_delete(request, id):
+
+    productos =delete_producto(id)
+    context = {
+        'producto_list': productos
+    }
+    return render(request, 'Producto/productos.html', context)
+
+def admin_list(request):
+    productos = get_productos()
+    context = {
+        'producto_list': productos
+    }
+    return render(request, 'Producto/productosAdmin.html', context)
+
